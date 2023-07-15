75c9548f9020c4ee81630720f51999b9c3acc085
Danil <57587300+iroxmega@users.noreply.github.com>
2023-07-15 18:48:36 +0400

    Rework

---
+++ js/burger.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("menuToggle").style.width = "80%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("menuToggle").style.width = "0%";
+}
